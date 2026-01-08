c3ff67c55e65e5cb464057652f75b27cd65286f4
tasmim20 <tasmimrahman2000@gmail.com>
2026-01-08 23:23:33 +0600

    update code

--- leap_year_checking.js
+++ leap_year_checking.js
@@ -1,5 +1,3 @@
 const isLeapYear = (year) => {
   return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 };
-console.log(isLeapYear(2020)); // true
-console.log(isLeapYear(1900)); // false
