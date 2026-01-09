404b2aec295dd08e2206e4f60f68e18a7ef3c103
tasmim20 <tasmimrahman2000@gmail.com>
2026-01-09 22:26:37 +0600

    new problem

--- leap_year_checking.js
+++ leap_year_checking.js
@@ -1,3 +1,6 @@
 const isLeapYear = (year) => {
   return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 };
+// const isLeapYear = (year) => {
+//   return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+// };
